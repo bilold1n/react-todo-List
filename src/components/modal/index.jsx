a87cfd8c8b4549989972d7f7686dab633b1897f1
bilold1n <babdumutalov436@gmail.com>
2024-05-02 22:25:32 +0500

    vazifa

---
+++ src/components/modal/index.jsx
@@ -0,0 +1,54 @@
+import { useState } from "react";
+export default function Modal({ closeModal, itemid, setdodos }) {
+  let [text, settext] = useState("");
+  function hendlsubmit(e) {
+    e.preventDefault();
+    setdodos((prev) =>
+      prev.map((item) => {
+        if (item.id === itemid) {
+          return { ...item, text };
+        }
+        return item;
+      })
+    );
+    settext("");
+    closeModal((prev) => {
+      return { show: false, todoid: null };
+    });
+  }
+  return (
+    <>
+      <div className="modal">
+        <div className="tepaorab">
+          <h2 className="edittitle">Edit Modal</h2>
+          <img
+            onClick={() =>
+              closeModal((prev) => {
+                return { ...prev, show: false, todoid: "" };
+              })
+            }
+            src="./src/assets/close.svg"
+            alt="close"
+          ></img>
+        </div>
+        <div className="pasorab">
+          <form className="editform" onSubmit={hendlsubmit}>
+            <input
+              placeholder="Edit Modal"
+              className="input editinput"
+              type="text"
+              value={text}
+              required
+              onChange={(e) => settext(e.target.value)}
+            />
+            <div className="btn-orab">
+              <button type="submit" className="btn editbtn">
+                Save Change
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+}
